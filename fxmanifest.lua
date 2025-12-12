7c6dd963367c470d7deeec2a47a16394b767fa0c
KraytonOG <116412443+KraytonOG@users.noreply.github.com>
2025-12-12 08:42:53 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'KraytonOG | Kray Scripts'
+description 'Moneybag on back when player has dirty money'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
